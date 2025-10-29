d57e7201d05a4aef5e8df595d1ad2b02151c67da
eschubert1 <eschustring@gmail.com>
2025-10-29 17:51:03 -0400

    Updating plotting code, adding lineplots for image freshness

--- src/dotplots.py
+++ src/dotplots.py
@@ -17,8 +17,8 @@ def make_exp1_dataframe(path):
         'Treatment' : exp_results[:,1],
         'Day 5 Weight' : exp_results[:,2],
         'Test Order' : exp_results[:,3],
-        'Filippo\'s Score' : [float(score) for score in exp_results[:,4]],
-        'Josh\'s Score' : [float(score) for score in exp_results[:,5]]
+        'Filippo\'s Score' : [float(score) for score in exp_results[:,5]],
+        'Josh\'s Score' : [float(score) for score in exp_results[:,6]]
     })
     exp_results['score'] = (exp_results['Filippo\'s Score'] + exp_results['Josh\'s Score'])/2
     
@@ -48,25 +48,33 @@ def make_exp2_dataframe(path):
         'Filippo\'s Score' : [float(score) for score in exp_results[:,11]]
     })
     exp_results['score'] = (exp_results['Filippo\'s Score'] +
-                            exp_results['Josh\'s Score'])/2
+                            exp_results['Mihir\'s Score'] +
+                            exp_results['Josh\'s Score'])/3
     
     return exp_results
 
 def plot_scores(df, path):
 
     groups = df.groupby('Treatment')
+    trt_labels = ['F+W', 'F+NW', 'NF+W', 'NF+NW']
     for name, group in groups:
         plt.plot(group.Treatment, group.score, marker='o', linestyle='', markersize=12, label=name)
 
-    plt.legend()
+    plt.legend(labels = trt_labels)
     plt.xlabel('Treatment')
+    plt.xticks(ticks = ["T1", "T2", "T3", "T4"], labels=trt_labels)
     plt.ylabel('Average Freshness Score')
 
     plt.savefig(path)
+    plt.clf()
 
 
 exp1_results = make_exp1_dataframe('data/raw_data/exp1_results.txt')
 exp2_results = make_exp2_dataframe('data/raw_data/exp2_smell_test_results.txt')
 combined_results = pd.concat([exp1_results[['Treatment', 'score']], 
                               exp2_results[['Treatment', 'score']]], ignore_index=True)
+
 plot_scores(combined_results, 'figures/average_freshness_all.pdf')
+plot_scores(combined_results, 'figures/average_freshness_all.png')
+plot_scores(exp1_results, "figures/average_freshness_exp1.pdf")
+plot_scores(exp2_results, "figures/average_freshness_exp2.pdf")
